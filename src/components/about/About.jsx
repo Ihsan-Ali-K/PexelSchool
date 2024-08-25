5177f09c20ab2582d7059f1eb9f9302361bc5407
Ihsan-Ali-K <ihsanalikhan39@gmail.com>
2024-08-25 15:44:59 +0500

    updated about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         <div className='position-relative image'>
           <img src={Image} />
           <div className=" text position-absolute top-0 d-flex align-items-center justify-content-center flex-column gap-1 ">
-            <h2><span>About</span> Le Groupe Scolaire LEPEX</h2>
+            <h2 className='text-center pt-5 pt-md-0'><span>About</span> Le Groupe Scolaire LEPEX</h2>
             <div className='border'></div>
             <div className='border-1'></div>
             <p className='mt-2 px-md-5'>Le Groupe Scolaire LEPEX is a hybrid school offering a learning curriculum in both French and English following the Ivorian and American Texan curricula. Our goal is to offer a fully bilingual school where the children will be well-versed in both languages as English is the most internationally spoken language in the world.</p>
